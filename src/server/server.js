ca419302c65d72274175e940a837c83d20de209d
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-03-06 01:11:09 -0500

    server is working now

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,16 @@ const app = express();
 const router = require("./api");
 const { User, Event, UserFriends, UserEvents } = require("./db/associations");
 const { Sequelize, DataTypes } = require("sequelize");
+const path = require('path');
+
+const dotenv = require('dotenv').config();
+if(dotenv.error) console.error(dotenv.error);
+
+const port = process.env.PORT;
+const pgUri = String(process.env.PGURI);
 
 const sequelize = new Sequelize(
-  "postgresql://postgres:postgres@planoramadb.cuqfjshz1zj6.us-east-2.rds.amazonaws.com:5432/",
+  pgUri,
   {
     dialectOptions: {
       ssl: {
@@ -17,5 +24,27 @@ const sequelize = new Sequelize(
 );
 
 app.use(express.json());
+
+
 app.use("/api", router);
-app.listen(3000, () => console.log("CONNECTED: listening on PORT", 3000));
+app.use(express.static(path.join(__dirname, '../', 'build')));
+
+
+
+app.use((req, res) => res.status(404).send('Error 404, path not found'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log : 'Express error handler caught unknown middleware error in server.js',
+    status: 400,
+    message: { err: 'An error occurred'} 
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.error(errorObj.log);
+  return res.status(errorObj.status || 500).json(errorObj.message);
+});
+
+
+// app.listen(3000, () => console.log("CONNECTED: listening on PORT", 3000));
+app.listen(port, () => console.log("CONNECTED: listening on PORT", port));
+module.exports = app;

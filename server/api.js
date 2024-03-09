f007557b9743b3812a992cc19b4556f172922b4b
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-03-09 13:11:58 -0500

    added routes for userFriends controller

--- server/api.js
+++ server/api.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const { eventController } = require("../server/controller/eventController");
 const { User, Event, UserFriends, UserEvents } = require("./db/associations");
 const { userController } = require('./controller/userController');
+const { userFriendsController } = require('./controller/userFriendsController');
 
+// user routes
 
 router.get("/v1/user", userController.getUser, (req, res) => {
   res.status(200).json(res.locals.user);
 });
 
 router.get("/v1/user/all", async (req,res) => {
-  console.log('connected to front end')
   const users = await User.findAll();
   res.status(200).send(users);
 })
@@ -36,6 +37,8 @@ router.patch("/v1/user", userController.updateUser, (req, res) => {
   res.status(200).send(res.locals.message)
 });
 
+// event routes
+
 router.get("/v1/event/:eventID", eventController.getEvent, (req, res, next) => {
   res.status(200).json(res.locals.event);
 });
@@ -49,5 +52,22 @@ router.put("/v1/event", eventController.updateEvent, (req, res, next) => {
 router.delete("/v1/event", eventController.deleteEvent, (req, res, next) => {
   res.status(200);
 });
+
+// userFriends controllers
+router.get("/v1/userFriends", userFriendsController.getFriendsList, (req, res) => {
+  console.log('got friends list')
+  res.status(200).json(res.locals.friends);
+})
+
+router.post("/v1/userFriends", userFriendsController.addFriend, (req, res) => {
+  console.log('userFriends addFriend succeeded')
+  res.status(200).json({"success": true});
+})
+
+router.delete("/v1/userFriends", userFriendsController.deleteFriend, (req, res) => {
+  console.log('deleted friend successfully');
+  res.status(200).json({"success": true});
+})
+
 
 module.exports = router;
